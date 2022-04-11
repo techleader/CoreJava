f8c3aee2a2434bbaa5a20ffa11c24ce5cf07aaab
techleader <jlsnayak1983@gmail.com>
2022-04-11 23:51:46 +0530

    Added Kafka Consumer - Refactoring

---
+++ src/main/java/com/learn/core/java/java8/FunctionExamples.java
@@ -0,0 +1,13 @@
+package com.learn.core.java.java8;
+
+import java.util.function.Function;
+
+public class FunctionExamples {
+    public static void main(String[] args) {
+
+        Function<Integer, Integer> add =  (i) -> i;
+        //add.apply()
+
+
+    }
+}
